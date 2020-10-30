7dfd4544b058b8e09f0feb32dfc345e7261caff9
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-10-30 21:23:16 +0000

    index.js returning sum from A to B, has been linted

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
+  if (fromN === toN) {
+    return toN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
   // Sum all the values from fromN up to toN
-}
+};
+
 
 module.exports = sum;
